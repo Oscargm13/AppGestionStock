b163e9685af1ee242bc7a0c4561ee1f27feacd82
Oscargm13 <oskitargm5@gmail.com>
2025-02-26 22:50:48 +0100

    Update VistaProductosGerente class and add SQL view
    - Added using directive for Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor.
    - Added region block with SQL script for creating VistaProductosGerente view.
    - Updated PrimaryKey attribute to include IdProveedor.
    - Renamed Nombre property to NombreProducto.
    - Added new properties: IdGerente, IdProveedor, and NombreProveedor.

--- AppGestionStock/Models/VistaProductosGerente.cs
+++ AppGestionStock/Models/VistaProductosGerente.cs
@@ -1,18 +1,47 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 
+#region
+/*CREATE VIEW VistaProductosGerente AS
+SELECT
+    p.IdProducto,
+    p.Nombre AS NombreProducto,
+    p.Precio,
+    p.Coste,
+    p.IdCategoria,
+    p.Imagen,
+    t.IdTienda,
+    t.Nombre AS NombreTienda,
+    pt.Cantidad AS StockTienda,
+    mt.IdUsuario AS IdGerente,
+    prov.IdProveedor,
+    prov.NombreEmpresa
+FROM
+    Productos p
+JOIN
+    ProductosTienda pt ON p.IdProducto = pt.IdProducto
+JOIN
+    Tiendas t ON pt.IdTienda = t.IdTienda
+JOIN
+    ManagersTiendas mt ON t.IdTienda = mt.IdTienda
+LEFT JOIN
+    ProductosProveedores pp ON p.IdProducto = pp.IdProducto
+LEFT JOIN
+    Proveedores prov ON pp.IdProveedor = prov.IdProveedor;*/
+#endregion
 namespace AppGestionStock.Models
 {
     [Table("VistaProductosGerente")]
-    [PrimaryKey(nameof(IdProducto), nameof(IdTienda))]
+    [PrimaryKey(nameof(IdProducto), nameof(IdTienda), nameof(IdProveedor))]
     public class VistaProductosGerente
     {
         [ForeignKey("IdProducto")]
         public int IdProducto { get; set; }
 
-        [Column("Nombre")]
-        public string Nombre { get; set; }
+        [Column("NombreProducto")]
+        public string NombreProducto { get; set; }
 
         [Column("Precio")]
         public decimal Precio { get; set; }
@@ -34,5 +63,14 @@ namespace AppGestionStock.Models
 
         [Column("StockTienda")]
         public int StockTienda { get; set; }
+
+        [Column("IdGerente")]
+        public int IdGerente { get; set; }
+
+        [ForeignKey("IdProveedor")]
+        public int IdProveedor { get; set; }
+
+        [Column("NombreProveedor")]
+        public string NombreProveedor { get; set; }
     }
 }
